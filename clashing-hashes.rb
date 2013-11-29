23a3171a782f0298d3086d12f05aeece6edc8b69
Elaine Looney <imalooney@gmail.com>
2013-11-29 12:17:34 -0600

    update stying and add styling to result page

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,23 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'twitter'
 
 get '/' do
-  'hello world'
+  	@name = "Elaine"
+  	@show_description = true
+  	erb :index
+end
+
+# Get request for the root url
+get '/' do
+  # your code goes here
+end
+
+# Post request for /twitter
+post '/twitter' do
+  # your code goes here
+  @first_hash = params[:first_hash] 
+  @second_hash = params[:second_hash]
+  erb :result
 end
---
+++ public/really_cool.js
@@ -0,0 +1,4 @@
+// $('#surprise').on('click', function(){
+// 	this.css('background;', '#000000;')
+// };
+// );
